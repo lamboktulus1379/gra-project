d0417ec0c36455474b94a2a2b82b27049142cd8c
Lambok Tulus <lamboktulus1379@gmail.com>
2025-05-10 15:25:05 +0700

    Added simple http handler that return Hello, World

--- main.go
+++ main.go
@@ -2,8 +2,25 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"net/http"
 )
 
+// helloHandler responds to requests with the hello world message
+func helloHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello, World!")
+}
+
 func main() {
-	fmt.Println("Hello, World!")
+	// Register the handler for the /hello endpoint
+	http.HandleFunc("/hello", helloHandler)
+
+	// Print a message indicating that the server is starting
+	fmt.Println("Starting server on :8080")
+
+	// Start the HTTP server on port 8080
+	err := http.ListenAndServe(":8080", nil)
+	if err != nil {
+		log.Fatalf("Failed to start server: %v", err)
+	}
 }
